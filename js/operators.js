4690fcc87fb9fa3b4b5d2972b97a06d9dc47237e
Santosh Kumar Ranawat <113606182+Santoshranawat@users.noreply.github.com>
2026-01-08 12:46:24 +0000

    learn operators

---
+++ js/operators.js
@@ -0,0 +1,48 @@
+/* operator is many type liek this:
+
+arthmetic Operator
+*/
+let add = 2 + 3; // addition
+let subs = 2 - 1; // subscription
+let mult = 2*3; // multiplication
+let divi = 15/3; // division
+let module = 21%3; // module
+
+// console.table([
+//     {addition: add, substraction: subs, multiplication: mult, Division: divi, Module: module}
+// ])
+
+// assignment operator 
+
+let onet = 20;
+onet *= 2; 
+onet += 2
+onet /=2
+// console.log(onet);
+
+// comparison operator
+let a = 12;
+let b = 10;
+console.log(a>b);
+let aa = "10";
+let bb = 10;
+console.log(15 === "15"); // if value same type same result is true
+console.log(15 == "15"); // if value same result is true
+
+// logical operator 
+
+let log = true ; let log2 = false;
+console.log(log && log2) // AND operator
+console.log(log || log2) // OR operator
+
+// ternury operator 
+
+let age = 18
+let check = age >= 20 ? "adult" : "Not Adult";
+console.log(check);
+
+// Unary Operators 
+let increment = 10
+console.log(++increment); // pre
+let dicrement = 10;
+console.log(dicrement--); // post
